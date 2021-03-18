2d1b55a4a82dd062d960928066cdeb3e86fee5a1
bellerophons-pegasus <bellerophons_pegasus@yahoo.de>
2021-03-18 14:45:20 +0100

    rename module

--- src/team14-software/io14.py
+++ src/team14-software/io14.py
@@ -1,4 +1,4 @@
-# I/O module
+# I/O module for team14-software
 
 import pandas as pd
 import numpy as np
@@ -8,11 +8,13 @@ def readfile(filepath, mode, separator='\t', datatype=float, skiprows=1):
 
     Arguments:
         filepath: path to the file including the filename
-        separator: separator used in the file (e.g. '\t'), can also be a regular expression like '    |   |  '
         mode:  indicates if data should be read into a dataframe ('df') or into a NumPy array ('npy')
+        separator: optional, separator used in the file (e.g. '\t'), can also be a regular expression like '    |   |  '
+        datatype: optional,
+        skiprows: optional
     """
     if mode == 'df':
         data = pd.read_table(filepath, separator, engine='python')
-    if mode == 'npa'
+    if mode == 'npa':
         data = np.loadtxt(filepath,datatype,skiprows)
     return data
