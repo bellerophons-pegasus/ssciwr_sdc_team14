38a46b75fc12f76b2caf161f9a4d84f2e8108c02
bellerophons-pegasus <bellerophons_pegasus@yahoo.de>
2021-03-28 15:41:56 +0200

    pytest filter14

--- src/team14-software/test/test2_filter14.py
+++ src/team14-software/test/test2_filter14.py
@@ -5,32 +5,46 @@ import pytest
 import pandas as pd
 import filter14 as f14
 
-class DF:
-        def __init__(self, testdict):
-            self.testdict = testdict
+class build_df:
+        def __init__(self):
+            pass
 
-        def init_df(self):
+        def init_df(self, testdict):
             test_df = pd.DataFrame(data=testdict)
             return test_df
 
 # runs this function before the test starts;
 # here a variable for testing is defined
-@pytest.fixture(testval)
-def test_df():
-    testval = build_testval({'col1': [1, 2], 'col2': [3, 4]})
-    return DF.init_df(testval)
+@pytest.fixture()
+def testval():
+    obj = build_df()
+    testval = obj.init_df({'col1': [1, 2], 'col2': [3, 4]})
+    return testval
+
+# runs this function before the test starts;
+# here another variable for testing is defined
+@pytest.fixture()
+def refval():
+    obj2 = build_df()
+    refval = obj2.init_df({'col1': [1, 2], 'col2': [3, 4]})
+    return refval
+
+@pytest.fixture()
+def emptyrefval():
+    obj3 = build_df()
+    emptyrefval = obj3.init_df({})
+    return emptyrefval
 
 
-
-def test_filterdata(testval, refval):
-     """Tests for filterdata"""
-     assert len(filterdata(testval)) == len(refval)
-#
-#         test1 = f14.filterdata(test_df)
-#         self.assertEqual(len(test1.columns), len(test_df.columns))
-#         test2 = f14.filterdata(test_df, 0.5)
-#         self.assertLess(len(test2.columns), len(test_df.columns))
-#         test3 = f14.filterdata(test_df, 0.5, ['col1', 'col2'])
-#         self.assertEqual(len(test3.columns), len(test_df.columns))
-#         test4 = f14.filterdata(test_df, 0.5, ['col1'])
-#         self.assertGreater(len(test4.columns), len(test2.columns))
+@pytest.mark.parametrize("threshold, columns", [(  0, []),
+                                                (0.5, ['col1', 'col2']),
+                                                (0.5, ['col1']),
+                                                (0.6, [])])
+def test_filterdata(testval, refval, emptyrefval, threshold, columns):
+    """Tests for filterdata"""
+    if len(columns)== 1:
+        assert len(f14.filterdata(testval,threshold, columns)) > len(emptyrefval)
+    elif threshold == 0.6:
+        assert len(f14.filterdata(testval,threshold, columns).columns) < len(refval.columns)
+    else:
+        assert len(f14.filterdata(testval,threshold, columns)) == len(refval)
