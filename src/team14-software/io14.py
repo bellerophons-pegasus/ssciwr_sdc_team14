b202b728caa1cd3bdca4b7f6c5b9b64f9927e8ee
bellerophons-pegasus <bellerophons_pegasus@yahoo.de>
2021-03-30 13:16:57 +0200

    linting

--- src/team14-software/io14.py
+++ src/team14-software/io14.py
@@ -1,21 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
 # io14.py
-# I/O module for team14-software
+
+"""
+Input and output for team14-software.
+
+Function readfile reads files.
+Function writefile writes given data to file.
+"""
+
 
 import pandas as pd
 import numpy as np
 
+
 def readfile(filepath, mode, separator='\t', datatype=float, skiprows=1):
-    """Reads a given (csv) file with the indicated separator.
+    """Read a given (csv) file with the indicated separator.
 
-        Args:
-            filepath (str): path to the file including the filename
-            mode (str):  indicates if data should be read into a Pandas dataframe ('df') or into a NumPy array ('npa')
-            separator (str): optional, separator used in the file (e.g. '\t'), can also be a regular expression like '    |   |  '
-            datatype (type): optional,
-            skiprows (int): optional
+    Args:
+        filepath (str): path to the file including the filename
+        mode (str): indicates if data should be read into a Pandas\
+                    dataframe ('df') or into a NumPy array ('npa')
+        separator (str): optional, separator used in the file\
+                         (e.g. '\t'), can also be a\
+                         regular expression like '    |   |  '
+        datatype (type): optional,
+        skiprows (int): optional
 
-        Returns:
-            dataframe, array: A structured data object, either Pandas dataframe or NumPy array
+    Returns:
+        dataframe, array: A structured data object, either \
+                          Pandas dataframe or NumPy array
     """
     if mode == 'df':
         data = pd.read_table(filepath, separator, engine='python')
@@ -23,13 +37,14 @@ def readfile(filepath, mode, separator='\t', datatype=float, skiprows=1):
         data = np.loadtxt(filepath, dtype=datatype, skiprows=skiprows)
     return data
 
-def writefile(data, filepath, encoding='utf-8', header=False):
-    """Writes a given dataframe to a csv file.
 
-        Args:
-            data: A dataframe to be parsed into a file
-            filepath (str): Path to the file including the filename
-            encoding (str): Encoding to be used for the file, defaults to 'utf-8'
-            header (boolean): Indicates if a header is included
+def writefile(data, filepath, encoding='utf-8', header=False):
+    """Write a given dataframe to a csv file.
+
+    Args:
+        data: A dataframe to be parsed into a file
+        filepath (str): Path to the file including the filename
+        encoding (str): Encoding to be used for the file, defaults to 'utf-8'
+        header (boolean): Indicates if a header is included
     """
     data.to_csv(filepath, encoding=encoding, header=header)
--- src/team14-software/statistics14.py
+++ src/team14-software/statistics14.py
@@ -1,57 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
 # statistics14.py
-# Module statistics for team14-software
 
-import pandas as pd
+"""
+Module for statistic analyses.
+
+Does correlation and euclidean distance.
+"""
+
+import pandas
+import numpy as np
+
 
 def correlatedata(data, corrmethod='pearson', dropcols=[]):
-    """Compute pairwise correlation of data with pandas.DataFrame.corr
+    """Compute pairwise correlation of data with \
+       pandas.DataFrame.corr.
 
     Args:
         data (pandas dataframe): Data to be correlated.
         corrmethod (str): Method to be used for correlation \
-        (pandas: pearson, kendall, spearman), defaults to 'pearson'
-        dropcols (list): A list with labels of columns to be dropped from the \
-        dataframe, defaults to []
+                          (pandas: pearson, kendall, spearman), \
+                          defaults to 'pearson'
+        dropcols (list): A list with labels of columns to be dropped\
+                         from the dataframe, defaults to []
     """
-
-    # correlate data
+    #  correlate data
     data = data.corr(method=corrmethod)
 
-    # drop given columns
+    #  drop given columns
     for i in dropcols:
         print('Removing column: {}'.format(i))
         datadrop = data.drop([i], axis=1)
         data = datadrop
 
-    # now collect superfluous cells to be removed
-    drop_values = set() # an unordered collection of items
-    cols = data.columns # get the column labels
+    #  now collect superfluous cells to be removed
+    drop_values = set()  # an unordered collection of items
+    cols = data.columns  # get the column labels
     for i in range(0, data.shape[1]):
-        for j in range(0, i+1): # get rid of all diagonal entries and the lower triangle
+        # get rid of all diagonal entries and the lower triangle
+        for j in range(0, i + 1):
             drop_values.add((cols[i], cols[j]))
     print('Values to drop')
     print(drop_values)
 
-    data = data.corr(method=corrmethod).unstack() # pivot the correlation matrix
-    data = data.drop(labels=drop_values).sort_values(ascending=False, key=lambda col: col.abs()) # sort by absolute values but keep sign
-   # display(data)
+    #  pivot the correlation matrix
+    data = data.corr(method=corrmethod).unstack()
+    #  sort by absolute values but keep sign
+    data = data.drop(labels=drop_values).sort_values(ascending=False,
+                                                     key=lambda col: col.abs())
 
     return data
 
-# used from team0
+
+#  used from team0
 def euclidean_distance(list_ref, list_comp, vectors):
-    """Calculates the Euclidean distance (L2 norm) between pairs of vectors.
+    """Calculate the Euclidean distance (L2 norm) between pairs of\
+     vectors.
 
     Args:
-        list_ref (integer list): A list with the indices of the reference vectors.
-        list_comp (integer list): A list with the indices of the vectors to\
-        compare to.
+        list_ref (integer list): A list with the indices of the\
+         reference vectors.
+        list_comp (integer list): A list with the indices of the\
+        vectors to compare to.
         data (numpy array): The data object.
 
     Returns:
-        numpy array: The Euclidean distance (L2 norm) for comparison vs. reference\
-        vectors."""
+        numpy array: The Euclidean distance (L2 norm) for comparison\
+         vs. reference vectors.
+    """
     distances = np.zeros(len(list_ref))
     for i in range(len(list_ref)):
-        distances[i] = np.linalg.norm(vectors[list_comp[i]] - vectors[list_ref[i]])
+        distances[i] = np.linalg.norm(
+            vectors[list_comp[i]] - vectors[list_ref[i]])
     return distances
