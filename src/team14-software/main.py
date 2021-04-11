55d45de1c28c2fe0cd7b9b167ce1f04fb235c4b2
bellerophons-pegasus <bellerophons_pegasus@yahoo.de>
2021-04-11 19:46:16 +0200

    visualise; main to join all tasks

---
+++ src/team14-software/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
+# main.py
+
+"""Executes all tasks and uses modules.
+
+If the package shall be run as a script, then this can be used.
+"""
+
+import os.path as path
+import numpy as np
+import pandas as pd
+import io14 as io14
+import filter14 as f14
+import statistics14 as s14
+import visualise14 as v14
+import numeric_methods as nm
+
+
+if __name__ == "__main__":
+    threshold = 1.0e-5
+    fileDir = path.join('..', '..', 'data')
+    outDir = path.join('..', 'pdfPlots')
+    # task 1
+    df_expect = io14.readfile(path.join(fileDir, 'expec.t'), 'df', '   ')
+    v14.plotlinlog(df_expect, 'Linear scale above, logarithmic scale below',
+                   outFile=path.join(outDir, 'task1Fig_unfiltered.pdf'))
+    filter_expect = f14.filterdata(df_expect, threshold)
+    # task 2
+    v14.plotlinlog(filter_expect, 'Linear scale above, logarithmic scale below',
+                   outFile=path.join(outDir, 'task2Fig_filtered.pdf'))
+    # task 3
+    df_npop = io14.readfile(path.join(fileDir, 'npop.t'), 'df',
+                            '    |   |  ')
+    filter_npop = f14.filterdata(df_npop, threshold, ['time'])
+    long_filter_npop = pd.melt(filter_npop, id_vars=['time'])
+    v14.plotseaborn(long_filter_npop, 'npop, filtered',
+                    outFile=path.join(outDir, 'task3Fig_filtered.pdf'))
+    corr_npop = s14.correlatedata(filter_npop, 'pearson', ['time'])
+    # task 4
+    io14.writefile(corr_npop, path.join(fileDir, 'npop_processed.csv'))
+    # task 5 (not complete)
+    arr = io.14readfile(path.join(fileDir, 'table.dat'), 'npa')
+    arr = np.nan_to_num(arr)
+    # main('efield.t', 'data/', 'output/')
+    # main('nstate_i.t', 'data/', 'output/')
--- src/team14-software/visualise14.py
+++ src/team14-software/visualise14.py
@@ -11,6 +11,8 @@ A collection of functions to plot data and save the resulting figure as a pdf.
 
 import matplotlib.pyplot as plt
 import seaborn as sb
+import pandas
+import numpy
 
 
 def plotlinlog(data, caption, figsize=(6, 9), outFile='linlogFig.pdf'):
@@ -22,11 +24,8 @@ def plotlinlog(data, caption, figsize=(6, 9), outFile='linlogFig.pdf'):
         figsize (tuple): optional, The size for the figure.
         outFile (str): optional, Path to the output file.
     """
-    fig, ax = plt.subplots(2, figsize=figsize)
-    fig.suptitle(caption)
-#    fig.suptitle('Linear scale above, logarithmic scale below')
-# for i in range(len(df_expect.keys())):
-
+    figll, ax = plt.subplots(2, figsize=figsize)
+    figll.suptitle(caption)
     for i in range(len(data.keys())):
         ax[0].plot(data[data.keys()[i]], label=data.keys()[i])
 
@@ -35,13 +34,14 @@ def plotlinlog(data, caption, figsize=(6, 9), outFile='linlogFig.pdf'):
     ax[1].set_yscale('symlog')
 
 #    fig.savefig('../pdfPlots/expect.pdf',format='pdf')
-    fig.savefig(outFile, format='pdf')
+    figll.savefig(outFile, format='pdf')
 
 
 def plotseaborn(data, caption, x='time', y='value', hue='variable',
                 outFile='sbFig.pdf'):
     """Plot data with seaborn.
 
+    TODO: not working properly.
     Args:
         data (pandas dataframe, long format): Data to be plotted in long format.
         caption (str): The caption for the figure.
@@ -51,17 +51,15 @@ def plotseaborn(data, caption, x='time', y='value', hue='variable',
         colors.
         outFile (str): optional, Path to the output file.
     """
-    sb.lineplot(x=x, y=y, hue=hue, data=data)
+    figsb = sb.lineplot(x=x, y=y, hue=hue, data=data)
     # place legend
-    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    plt.show()
-    plt.savefig(outFile)
+    figsb.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    figsb.figure.savefig(outFile)
 
 
 def plotplt(data, caption, mode, x='time', y='value', hue='variable',
             outFile='pltFig.pdf'):
     """Plot data with matplotlib.
-
     Args:
         data (pandas dataframe, long format): Data to be plotted.
         caption (str): The caption for the figure.
@@ -72,13 +70,14 @@ def plotplt(data, caption, mode, x='time', y='value', hue='variable',
         colors.
         outFile (str): optional, Path to the output file.
     """
+
     if mode == 'bar':
         xax = range(0, len(data))
-        plt.bar(xax, data, label=caption)
+        figplt = plt.bar(xax, data, label=caption)
     if mode == 'plot':
-        plt.plot(data, label=caption)
-    plt.xlabel(x)
-    plt.ylabel(y)
-    plt.legend()
-    plt.show()
-    plt.savefig(outFile)
+        figplt = plt.plot(data, label=caption)
+    figplt.xlabel(x)
+    figplt.ylabel(y)
+    figplt.legend()
+    figplt.show()
+    figplt.savefig(outFile)
